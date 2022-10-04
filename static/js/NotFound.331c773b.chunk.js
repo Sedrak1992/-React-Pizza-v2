d590be3803e14a286b120316663aea998f1570f7
Sedrak Qocharyan <sedrakqocharyan@gmail.com>
2022-10-04 23:27:12 +0400

    Updates

---
+++ static/js/NotFound.331c773b.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkpppppp=self.webpackChunkpppppp||[]).push([[187],{4949:function(p,u,e){e.r(u);e(2791);var n=e(9931),r=e(184);u.default=function(){return(0,r.jsx)(n.vr,{})}}}]);
+//# sourceMappingURL=NotFound.331c773b.chunk.js.map
